8157c25b966be8b901b106f0f6a5626ba88e9c10
Ameer <233405116@ogrenci.ibu.edu.tr>
2024-11-30 09:28:31 +0300

    added tests

---
+++ tests/ders1/AccountTest.java
@@ -0,0 +1,22 @@
+package ders1;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AccountTest {
+
+	@Test
+	public void test() {
+		Account k1 = new Account("Ahmet", 500);
+		Account k2 = new Account("Mehmet", 2500);
+		assertEquals("Ahmet", k1.getName());
+		assertEquals("Mehmet", k2.getName());
+		assertEquals(500, k1.getBakiye());
+		assertEquals(2500, k2.getBakiye());
+		k1.depositMoney(50);
+		assertEquals(550, k1.getBakiye());
+		assertEquals(2500, k2.getBakiye());
+	}
+
+}
